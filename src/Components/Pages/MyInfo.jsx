10bc36694fb7ae135e41c7587e7dc00ad3c003cc
Sri Chanakya Yennana <31926762+srichanakya@users.noreply.github.com>
2024-07-13 12:12:20 -0500

    Update MyInfo.jsx

--- src/Components/Pages/MyInfo.jsx
+++ src/Components/Pages/MyInfo.jsx
@@ -5,7 +5,7 @@ export default function MyInfo() {
     <div className='container mt-5'>
 
 <h2 style={{textAlign:'center'}}>My Info</h2>
-<h4 style={{textAlign:'center'}}>Sri Chanakya Chowdary Babu Yennana</h4>
+<h4 style={{textAlign:'center'}}> Place holder Name</h4>
 <h5 style={{textAlign:'center'}}>Role : Java Developer / SDE / QA / SDET</h5>
 
 
